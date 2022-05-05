d73e989c95ad2cfbf19065f443b242ccf70fb3d2
kraftcolor <grate-forked0f@icloud.com>
2022-05-05 13:59:08 +0530

    Title and excerpt can include HTML markup

--- src/preview.js
+++ src/preview.js
@@ -65,7 +65,7 @@ export default function Preview( { countryCode, relatedPosts } ) {
 										} }
 									/>
 
-									<p
+									<summary
 										className="excerpt"
 										dangerouslySetInnerHTML={ {
 											__html: DOMPurify.sanitize(
